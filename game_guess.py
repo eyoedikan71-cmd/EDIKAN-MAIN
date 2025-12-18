8f78604754a476e67102e6c588de5d528d26e2a9
eyoedikan <eyoedikan71@gmail.com>
2025-12-18 15:50:39 +0100

    This commit involves the guessing game with an input updated from this same file which was under mybranch

--- game_guess.py
+++ game_guess.py
@@ -21,52 +21,94 @@ import random
 
 parser = argparse.ArgumentParser(description="A number guessing game.")
 parser.add_argument("name",type=str,help="Your name")
+args = parser.parse_args()
+
+print(f"Hello {args.name}! Welcome to the Number Guessing Game! \nI'm thinking of a number between 1 and 100.")
 
 # args = parser.parse_args()
 # parser = argparse.ArgumentParser(description="Choose a level of the game")
-parser.add_argument("--difficulty",type=str,choices=["easy", "medium", "hard"],
-        required=True,
-        help="Choose difficulty level: easy, medium, hard")
-parser.add_argument(
-    "--guesses",
-    nargs="+",
-    type=int,
-    required=True,
-    help="List of guesses the user inputs"
-)
-args = parser.parse_args()
+# parser.add_argument("--difficulty",type=str,choices=["easy", "medium", "hard"],
+#         required=True,
+#         help="Choose difficulty level: easy, medium, hard")
+difficulty = input("Choose level of difficulty[easy, medium, hard]: ")
+print(difficulty)
+print(f"Difficulty: {difficulty} which is what you chosed")  
 
+# parser.add_argument(
+#     "--guesses",
+#     nargs="+",
+#     type=int,
+#     required=True,
+#     help="List of guesses the user inputs"
+# )
+# args = parser.parse_args()
 levels = {
-    "easy": 10,
-    "medium": 7,
-    "hard": 5
-}
-attempts = levels.get(args.difficulty)
+"easy": 10,
+"medium": 7,
+"hard": 5}
+if difficulty not in levels:
+    print("Please either choose easy, medium, or hard.")
+
+# attempts = levels.get(args.difficulty)
+attempts = levels[difficulty]
 number = random.randint(1, 100)
-attempt_count = 0
-print(f"Hello {args.name}! Welcome to the Number Guessing Game! \nI'm thinking of a number between 1 and 100.")
-print(f"Difficulty: {args.difficulty} which is what you chosed")  
-
-
 print("\nLet's begin the game!")
-for guess in args.guesses:
-    if attempt_count >= attempts:
-        break
+attempt_count = 0
+# print(f"Hello {args.name}! Welcome to the Number Guessing Game! \nI'm thinking of a number between 1 and 100.")
+# print(f"Difficulty: {args.difficulty} which is what you chosed")  
+# print(f"Difficulty: {difficulty} which is what you chosed")  
 
+
+#print("\nLet's begin the game!")
+while attempt_count < attempts:
+    guess = input("what is your guess? ")
+    try:
+        guess = int(guess)
+    except ValueError:
+        print("invalid number pls try again!")
+        continue
     attempt_count += 1
     print(f"Attempt {attempt_count}/{attempts}: You guessed {guess}")
-
+    # continue
     if guess == number:
-        print(f"\nCongratulations! The number was {number}.")
+        print(f"\nCongratulations! {args.name} The number was {number}.")
         print(f"You won in {attempt_count} attempts.")
         break
-
+    
     elif guess > number:
         print("Too high")
     else:
         print("Too low")
 
 if attempt_count >= attempts and guess != number:
-    print(f"\nOut of attempts! The number was {number}.")
+    print(f"\nOut of attempts! The number was {number}.") 
+    # except ValueError:
+    #     print("Please enter a valid number.")
+        # continue
+
+
+#guess = input("what are all your guesses? ")
+#print(guess)
+#for guess in args.guesses:
+#for user_guess in guess:
+# #guess = int(user_guess)
+    #if attempt_count >= attempts:
+        #break
+
+# attempt_count += 1
+# print(f"Attempt {attempt_count}/{attempts}: You guessed {guess}")
+
+# if guess == number:
+#     print(f"\nCongratulations! The number was {number}.")
+#     print(f"You won in {attempt_count} attempts.")
+# # break
+
+# elif guess > number:
+#     print("Too high")
+# else:
+#     print("Too low")
+
+# if attempt_count >= attempts and guess != number:
+#     print(f"\nOut of attempts! The number was {number}.")
 
 
